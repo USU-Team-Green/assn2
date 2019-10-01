79aebd1e972bedd5a0321cf892ae120cd788df4a
Ryan Ferguson <ryan28561@gmail.com>
2019-09-30 19:13:53 -0600

    added code for generating keys

--- task4/key_gen.py
+++ task4/key_gen.py
@@ -1,6 +1,26 @@
+import math
+import base64
+import sympy
+import os
 
+def lcm(a, b):
+    return a * b // math.gcd(a, b)
+
+def inverse(x, m): # for getting modular inverse (d)
+    a, b, u = 0, m, 1
+    while x > 0:
+        q = b // x
+        x, a, b, u = b % x, u, x, a - q * u
+    if b == 1:
+        return a % m
+    else:
+        print('something went wrong')
 
 
 def generate_keys():
-    # TODO: Actually generate the keys here
-    return ('public_key', 'private_key')
+    p, q = sympy.nextprime(int.from_bytes(os.urandom(15), byteorder='big')), sympy.nextprime(int.from_bytes(os.urandom(15), byteorder='big'))
+    n = p * q
+    lambda_n = lcm(p - 1, q - 1)
+    e = 65537
+    d = inverse(e, lambda_n)
+    return str(n) + "$" + str(e), str(d)
